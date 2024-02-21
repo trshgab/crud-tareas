de393f97faad28cf723cec50e4fc461f950e046c
trshgab <gmrgabo@gmail.com>
2024-02-21 16:07:15 -0300

    lineas inncesarias del login eliminadas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,11 +33,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+                <!-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Olvidaste tu contraseña?') }}
                     </a>
-                @endif
+                @endif -->
 
                 <x-button class="ms-4">
                     {{ __('Iniciar Sesion') }}
