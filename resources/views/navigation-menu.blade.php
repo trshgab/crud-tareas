2965a109ca67cca3f0760f33bffd6617959c9fce
trshgab <gmrgabo@gmail.com>
2024-02-21 16:06:36 -0300

    solucion a ver perfil

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -67,9 +67,9 @@
                                 <span class="inline-flex rounded-md">
                                     <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
                                         {{ auth()->user()->roles->implode('name', ', ') }}
-                                        <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <!-- <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-                                        </svg>
+                                        </svg> -->
                                     </button>
                                 </span>
                             </x-slot>
@@ -126,7 +126,7 @@
                             {{ __('Configurar Perfil') }}
                         </div>
             
-                        <x-dropdown-link href="{{ route('profile.show') }}">
+                        <x-dropdown-link href="{{ route('users.show', Auth::user()->id) }}">
                             {{ __('Perfil') }}
                         </x-dropdown-link>
             
